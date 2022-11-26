f7219f5c832228c730259ca0e075ebb2d9148834
Karen <karenjawane28@gmail.com>
2022-11-26 17:48:09 -0300

    first-commit

---
+++ ex1.js
@@ -0,0 +1,6 @@
+
+if (num >= 10) {
+    console.log("È maior ou igual que 10")
+}else {
+    console.log("È menor que 10")
+}
---
+++ ex2.js
@@ -0,0 +1,9 @@
+if (num <=10){
+    console.log("o valor é menor ou igual que 10")
+}else if (num <=20){
+    console.log("O valor é maior que 10 e menor ou igual que 20")
+}else if (num <=30){
+    console.log("O valor é maior que 20 e menor ou igual que 30")
+}else if (num >30){
+    console.log("O valor é maior que 30")
+}
